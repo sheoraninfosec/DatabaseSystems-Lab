9c464aeaee2193b7b20d5d397385d08739c9e6d6
Jigesh Sheoran <106379604+sheoraninfosec@users.noreply.github.com>
2025-08-18 21:55:56 +0530

    Update and rename sample-data.sql to sample_data.sql

--- Lab1/sample_data.sql
+++ Lab1/sample_data.sql
@@ -16,26 +16,26 @@ INSERT INTO Instructor (FirstName, LastName, DeptCode, Email, Phone) VALUES
 INSERT INTO Student (StudentNumber, SSN, FirstName, LastName, CurrentStreet, CurrentCity, CurrentState, CurrentZipCode,
 CurrentPhone, PermanentStreet, PermanentCity, PermanentState, PermanentZipCode, PermanentPhone, BirthDate, Sex, Class, DegreeProgram, MajorDeptCode, MinorDeptCode) VALUES
 (1001, '123456789', 'Jigesh', 'Sheoran', 'Plot 180 P&B Society', 'Dehradun', 'UK', '248007', '111-111-0000',
-'Plot 180, P&B Society', 'Dehradun', 'UK', '248007', '111-111-0000', '2000-01-01', 'M', 'Freshman', 'MTECH', 'CSF', 'MATH'),
+'Plot 180, P&B Society', 'Dehradun', 'UK', '248007', '111-111-0000', '2000-01-01', 'M', 'Freshman', 'MS', 'CSF', 'MATH'),
 (1002, '987654321', 'Raghav', 'Rajput', 'Plot 180, P&B Society', 'Dehradun', 'UK', '248007', '222-222-0000',
-'Plot 180, P&B Society', 'Dehradun', 'UK', '248007', '222-222-0000', '2001-01-01', 'M', 'Freshman', 'MTECH', 'CS', NULL),
+'Plot 180, P&B Society', 'Dehradun', 'UK', '248007', '222-222-0000', '2001-01-01', 'M', 'Freshman', 'BS', 'CS', NULL),
 (1003, '555667777', 'William', 'Shakespeare', 'Plot 181 P&B Society', 'Dehradun', 'UK', '248007', '333-333-0000',
-'11 Rajpur Road', 'Dehradun', 'UK', '248001', '333-333-0001', '2002-01-01', 'M', 'Senior', 'BTECH', 'MATH', 'CSF');
+'11 Rajpur Road', 'Dehradun', 'UK', '248001', '333-333-0001', '2002-01-01', 'M', 'Senior', 'BA', 'MATH', 'CSF');
 
 -- Insert Courses
 INSERT INTO Course (CourseNumber, CourseName, Description, SemesterHours, Level, DeptCode) VALUES
 ('CS101', 'Intro to Programming', 'Basics of programming in Python', 3, 'Undergraduate', 'CS'),
-('MATH201', 'Calculus I', 'Differential and Integral Calculus', 4, 'Undergraduate', 'MATH'),
-('ENG301', 'English Literature', 'Survey of classic English texts', 3, 'Undergraduate', 'ENG');
+('CSF201', 'Network Security', 'Principles of cybersecurity and networking', 3, 'Undergraduate', 'CSF'),
+('MATH301', 'Linear Algebra', 'Matrix theory and linear algebra', 4, 'Undergraduate', 'MATH');
 
 -- Insert Sections
 INSERT INTO Section (CourseNumber, Semester, Year, SectionNumber, InstructorID) VALUES
-('CS101', 'Fall', 2023, 1, 1),
-('MATH201', 'Fall', 2023, 1, 2),
-('ENG301', 'Fall', 2023, 1, 3);
+('CS101', 'Fall', 2023, 1, 2),   -- Raghav teaches CS
+('CSF201', 'Fall', 2023, 1, 1),  -- Jigesh teaches CSF
+('MATH301', 'Fall', 2023, 1, 3); -- William teaches Math
 
 -- Insert Grade Reports
 INSERT INTO GradeReport (StudentNumber, SectionID, LetterGrade, NumericGrade) VALUES
-(1001, 1, 'A', 4),
-(1002, 3, 'B', 3),
-(1003, 2, 'A', 4);
+(1001, 2, 'A', 4),  -- Jigesh (student) in Cyber Security
+(1002, 1, 'B', 3),  -- Raghav (student) in CS
+(1003, 3, 'A', 4);  -- William (student) in Math
